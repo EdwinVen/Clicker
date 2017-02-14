106c38a7236c93bce1047628da2fc900d76927bd
Edwins Ventura <eduinventura@gmail.com>
2017-02-14 16:31:31 -0500

    upload everything

---
+++ ConsoleClicker/Click.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+
+namespace ConsoleClicker {
+    enum ClickAction {
+        MOUSEEVENTF_ABSOLUTE = 0x8000,
+        MOUSEEVENTF_MOVE = 0x0001,
+        MOUSEEVENTF_LEFTDOWN = 0x02,
+        MOUSEEVENTF_LEFTUP = 0x04,
+        MOUSEEVENTF_RIGHTDOWN = 0x08,
+        MOUSEEVENTF_RIGHTUP = 0x10,
+        MOUSEEVENTF_MIDDLEDOWN = 0x0020,
+        MOUSEEVENTF_MIDDLEUP = 0x0040,
+        MOUSEEVENTF_WHEEL = 0x0800,
+        MOUSEEVENTF_HWHEEL = 0x01000,
+        MOUSEEVENTF_XDOWN = 0x0080,
+        MOUSEEVENTF_XUP = 0x0100
+    }
+
+    class Click : IPerformable{
+        [DllImport("user32.dll", CharSet = CharSet.Auto, CallingConvention = CallingConvention.StdCall)]
+        public static extern void mouse_event(int dwFlags, int dx, int dy, int cButtons, int dwExtraInfo);
+
+        #region consts
+        private const int MOUSEEVENTF_ABSOLUTE = 0x8000;
+        private const int MOUSEEVENTF_MOVE = 0x0001;
+        private const int MOUSEEVENTF_LEFTDOWN = 0x02;
+        private const int MOUSEEVENTF_LEFTUP = 0x04;
+        private const int MOUSEEVENTF_RIGHTDOWN = 0x08;
+        private const int MOUSEEVENTF_RIGHTUP = 0x10;
+        private const int MOUSEEVENTF_MIDDLEDOWN = 0x0020;
+        private const int MOUSEEVENTF_MIDDLEUP = 0x0040;
+        private const int MOUSEEVENTF_WHEEL = 0x0800;
+        private const int MOUSEEVENTF_HWHEEL = 0x01000;
+        private const int MOUSEEVENTF_XDOWN = 0x0080;
+        private const int MOUSEEVENTF_XUP = 0x0100;
+        #endregion consts
+
+        public ClickAction Type;
+
+        private int delayAfterClick;
+        public int DelayAfterClick {
+            get {
+                return delayAfterClick;
+            }
+            set { if (value >= 0) delayAfterClick = value; }
+        }
+
+        public int X { get; set; }
+        public int Y { get; set; }
+
+        public Click(): this(ClickAction.MOUSEEVENTF_LEFTDOWN, 0, 0, 0) {
+
+        }
+
+        public Click(ClickAction newType, int newX, int newY, int newDelay) {
+            Type = newType;
+            X = newX;
+            Y = newY;
+            DelayAfterClick = newDelay;
+        }
+
+        public void Perform() {
+            mouse_event((int)Type, X, Y, 0, 0);
+            Thread.Sleep(DelayAfterClick);
+            
+        }
+    }
+}
---
+++ ConsoleClicker/Program.cs
@@ -0,0 +1,180 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+using System.Runtime.InteropServices;
+using System.Windows.Input;
+using System.Drawing;
+
+namespace ConsoleClicker {
+    class Program {
+
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, CallingConvention = CallingConvention.StdCall)]
+        public static extern void mouse_event(int dwFlags, int dx, int dy, int cButtons, int dwExtraInfo);
+
+        [DllImport("User32.dll",
+               EntryPoint = "GetSystemMetrics",
+               CallingConvention = CallingConvention.Winapi)]
+        internal static extern int InternalGetSystemMetrics(int value);
+
+
+        private const int MOUSEEVENTF_ABSOLUTE = 0x8000;
+        private const int MOUSEEVENTF_MOVE = 0x0001;
+        private const int MOUSEEVENTF_LEFTDOWN = 0x02;
+        private const int MOUSEEVENTF_LEFTUP = 0x04;
+        private const int MOUSEEVENTF_RIGHTDOWN = 0x08;
+        private const int MOUSEEVENTF_RIGHTUP = 0x10;
+        private const int MOUSEEVENTF_MIDDLEDOWN = 0x0020;
+        private const int MOUSEEVENTF_MIDDLEUP = 0x0040;
+        private const int MOUSEEVENTF_WHEEL = 0x0800;
+        private const int MOUSEEVENTF_HWHEEL = 0x01000;
+        private const int MOUSEEVENTF_XDOWN = 0x0080;
+        private const int MOUSEEVENTF_XUP = 0x0100;
+
+        static void Main(string[] args) {
+            Point p = System.Windows.Forms.Control.MousePosition;
+
+            int screenWidth = InternalGetSystemMetrics(0);
+            int screenHeight = InternalGetSystemMetrics(1);
+
+            Thread.Sleep(5000);
+            //int X = (int)System.Math.Round((p.X + 50) * 65536.0 / screenWidth);
+            //int Y = (int)System.Math.Round( (p.Y + 50) * 65536.0 / screenHeight);
+
+            int X = (int)System.Math.Round((p.X ) * 65536.0 / screenWidth);
+            int Y = (int)System.Math.Round((p.Y ) * 65536.0 / screenHeight);
+
+            //mouse_event(MOUSEEVENTF_LEFTDOWN | MOUSEEVENTF_LEFTUP, X, Y, 0, 0);
+            /*mouse_event(MOUSEEVENTF_LEFTDOWN, X, Y, 0, 0);
+            mouse_event(MOUSEEVENTF_MOVE, 50, 50, 0, 0);
+            mouse_event(MOUSEEVENTF_LEFTUP, X, Y, 0, 0);*/
+
+            #region drawSquare
+            //mouse_event(MOUSEEVENTF_LEFTDOWN, X, Y, 0, 0);
+
+            //int delay = 100;
+            //int length = 50;
+            //while (length > 0) {
+            //    for (int i = 0; i < length; i++) {
+            //        Thread.Sleep(delay);
+            //        mouse_event(MOUSEEVENTF_MOVE, 5, 0, 0, 0);
+            //    }
+            //    length -= 5;
+            //    delay -= 5;
+
+            //    for (int i = 0; i < length; i++) {
+            //        Thread.Sleep(delay);
+            //        mouse_event(MOUSEEVENTF_MOVE, 0, 5, 0, 0);
+            //    }
+            //    delay -= 5;
+
+            //    for (int i = 0; i < length; i++) {
+            //        Thread.Sleep(delay);
+            //        mouse_event(MOUSEEVENTF_MOVE, -5, 0, 0, 0);
+            //    }
+            //    length -= 5;
+            //    delay -= 5;
+
+            //    for (int i = 0; i < length; i++) {
+            //        Thread.Sleep(delay);
+            //        mouse_event(MOUSEEVENTF_MOVE, 0, -5, 0, 0);
+            //    }
+            //    delay -= 5;
+            //}
+
+            ////mouse_event(MOUSEEVENTF_MOVE, 50, 50, 0, 0);
+
+            //mouse_event(MOUSEEVENTF_LEFTUP, X, Y, 0, 0);
+
+            #endregion drawSquare
+
+            #region drawSquarePerform
+            //mouse_event(MOUSEEVENTF_LEFTDOWN, X, Y, 0, 0);
+
+            //int delay = 100;
+            //int length = 50;
+            //Click leftRight = new Click(ClickAction.MOUSEEVENTF_MOVE, 5, 0, 0);
+            //Click upDown = new Click(ClickAction.MOUSEEVENTF_MOVE, 0, 5, 0);
+            //Click rightLeft = new Click(ClickAction.MOUSEEVENTF_MOVE, -5, 0, 0);
+            //Click downUp = new Click(ClickAction.MOUSEEVENTF_MOVE, 0, -5, 0);
+
+            //while (length > 0) {
+            //    for (int i = 0; i < length; i++) {
+            //        Thread.Sleep(delay);
+            //        leftRight.Perform();
+            //    }
+            //    length -= 5;
+            //    delay -= 5;
+
+            //    for (int i = 0; i < length; i++) {
+            //        Thread.Sleep(delay);
+            //        upDown.Perform();
+            //    }
+            //    delay -= 5;
+
+            //    for (int i = 0; i < length; i++) {
+            //        Thread.Sleep(delay);
+            //        rightLeft.Perform();
+            //    }
+            //    length -= 5;
+            //    delay -= 5;
+
+            //    for (int i = 0; i < length; i++) {
+            //        Thread.Sleep(delay);
+            //        downUp.Perform();
+            //    }
+            //    delay -= 5;
+            //}
+
+            ////mouse_event(MOUSEEVENTF_MOVE, 50, 50, 0, 0);
+
+            //mouse_event(MOUSEEVENTF_LEFTUP, X, Y, 0, 0);
+
+            #endregion drawSquarePerform
+
+            #region writeE
+
+            List<Click> clickList = new List<Click>();
+            clickList.Add(new Click(ClickAction.MOUSEEVENTF_LEFTDOWN, 1000, 1000, 500));
+            clickList.Add(new Click(ClickAction.MOUSEEVENTF_MOVE, 0, 100, 500));
+            clickList.Add(new Click(ClickAction.MOUSEEVENTF_LEFTUP, 0, 0, 500));
+
+            clickList.Add(new Click(ClickAction.MOUSEEVENTF_MOVE, 0, -100, 500));
+
+            clickList.Add(new Click(ClickAction.MOUSEEVENTF_LEFTDOWN, 0, 0, 500));
+            clickList.Add(new Click(ClickAction.MOUSEEVENTF_MOVE, 50, 0, 500));
+            clickList.Add(new Click(ClickAction.MOUSEEVENTF_LEFTUP, 0, 0, 500));
+
+            clickList.Add(new Click(ClickAction.MOUSEEVENTF_MOVE, -50, 50, 500));
+
+            clickList.Add(new Click(ClickAction.MOUSEEVENTF_LEFTDOWN, 0, 0, 500));
+            clickList.Add(new Click(ClickAction.MOUSEEVENTF_MOVE, 50, 0, 500));
+            clickList.Add(new Click(ClickAction.MOUSEEVENTF_LEFTUP, 0, 0, 500));
+
+            clickList.Add(new Click(ClickAction.MOUSEEVENTF_MOVE, -50, 50, 500));
+
+            clickList.Add(new Click(ClickAction.MOUSEEVENTF_LEFTDOWN, 0, 0, 500));
+            clickList.Add(new Click(ClickAction.MOUSEEVENTF_MOVE, 50, 0, 500));
+            clickList.Add(new Click(ClickAction.MOUSEEVENTF_LEFTUP, 0, 0, 500));
+
+            foreach (Click c in clickList) {
+                c.Perform();
+            }
+
+
+            #endregion writeE
+
+            Thread.Sleep(3000);
+
+            Console.WriteLine($"Screen width: {screenWidth}   Screen height: {screenHeight}");
+            Console.WriteLine($"X: {X}   Y: {Y}");
+
+            Console.Read();
+        }
+
+        
+    }
+}
